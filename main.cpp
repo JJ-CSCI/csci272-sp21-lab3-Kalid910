ef513b3a6ec01eb1b7c6e38af83aa03fe1168031
kidkalid <kidkalid@gmail.com>
2021-03-02 20:34:29 +0000

    Complete

--- main.cpp
+++ main.cpp
@@ -2,11 +2,61 @@
 //  DO NOT MODIFY NEXT LINE
 //------------------------------
 #include "catch.hpp"
+#include <iostream>
 //------------------------------
+using namespace std;
 
 // Put the assignment code here
 
 class Quaternion {
+private:
+    double a, b, c, d;
+
+public: 
+
+    Quaternion(double one, double two, double three, double four) {
+        a = one;
+        b = two;
+        c = three;
+        d = four;
+    }
+
+     bool operator==(const Quaternion& q) const {
+        return ((getA() == q.getA()) && (getB() == q.getB()) && (getC() == q.getC()) && (getD() == q.getD()));
+    }
+    
+     Quaternion operator+(const Quaternion& q) const {
+        Quaternion newQ(getA() + q.getA(), getB() + q.getB(), getC() + q.getC(), getD() + q.getD());
+        return newQ;
+    }
+
+     Quaternion operator-(const Quaternion& q) const {
+        Quaternion newQ(getA() - q.getA(), getB() - q.getB(), getC() - q.getC(), getD() - q.getD());
+        return newQ;
+    }
+
+     Quaternion operator*(const double val) const {
+        Quaternion newQ(getA() * val, getB() * val, getC() * val, getD() * val);
+        return newQ;
+    }
+
+    double getA() const {
+        return a;
+    }
+    
+    double getB() const {
+        return b;
+    }
+
+    double getC() const {
+        return c;
+    }
+
+    double getD()  const {
+        return d;
+    }
+
+
 };
 
 //------------------------------
